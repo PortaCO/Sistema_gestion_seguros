817b7d2a031aed82ce785a33ff83e0728d54f133
Christian <c.orihuelaporta02@gmail.com>
2024-12-12 21:57:40 -0500

    alter table

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -5,6 +5,7 @@
 USE db_gestion_seguros
 GO
 
+-- TABLA Agente
 CREATE TABLE AGENTE(
 ID INT PRIMARY KEY IDENTITY(1,1),
 nombre VARCHAR(100) NOT NULL,
@@ -15,6 +16,11 @@ zona VARCHAR(50) NOT NULL
 );
 GO
 
+--restringiendo las zonas en lima - Peru
+ALTER TABLE AGENTE
+ADD CONSTRAINT check_zona CHECK (zona in ('Lima Norte', 'Lima Sur', 'Lima Este', 'Lima Oeste', 'Lima Centro'))
+
+-- TABLA tipo_poliza
 CREATE TABLE TIPO_POLIZA(
 ID INT PRIMARY KEY IDENTITY(1,1),
 nombre_tipo VARCHAR(50) NOT NULL,
@@ -22,6 +28,7 @@ descripcion TEXT NULL
 );
 GO
 
+-- TABLA metodo_pago
 CREATE TABLE METODO_PAGO(
 ID INT PRIMARY KEY IDENTITY(1,1),
 nombre_metodo VARCHAR(50) NOT NULL,
