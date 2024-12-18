a46f559308583c40a86b1a14e6175bd1cd048b39
Christian <c.orihuelaporta02@gmail.com>
2024-12-18 18:50:28 -0500

    update

--- consultas/consultas_select.sql
+++ consultas/consultas_select.sql
@@ -7,5 +7,21 @@ SELECT
 FROM CLIENTE
 WHERE nombre in ('Ricardo','Diana');
 
+/* Encontrar filas que contienen un valor como parte de una cadena
+Seleccionar todos los apellido de los clientes que contengan la 
+cadena "ez"
+*/
+SELECT  
+	CONCAT(nombre,' ',apellido) as 'Nombre Completo', 
+	DNI, correo  
+FROM CLIENTE
+WHERE apellido LIKE('%ez%');
+
+-- Los apellidos que empiezen con R o terminen con S
+SELECT  
+	CONCAT(nombre,' ',apellido) as 'Nombre Completo', 
+	DNI, correo  
+FROM CLIENTE
+WHERE apellido LIKE ('R%') OR apellido LIKE ('%S');
 
 select * from cliente
