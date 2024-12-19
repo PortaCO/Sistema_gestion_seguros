3f75fb232f71a493c81420c1e00339c143b23777
Christian <c.orihuelaporta02@gmail.com>
2024-12-18 21:29:36 -0500

    update

--- consultas/consultas_select.sql
+++ consultas/consultas_select.sql
@@ -65,3 +65,77 @@ WHERE
 -- Encontrar filas que no esten en una lista
 SELECT * FROM CLIENTE
 WHERE DNI NOT IN ('12345678', '45678901', '34561210')
+
+
+/* Funciones matematicas */
+
+-- Funcion SUM
+SELECT 
+	ID_CLIENTE, 
+	SUM(monto_asegurado) AS 'Monto total'
+FROM POLIZA
+GROUP BY ID_CLIENTE
+ORDER BY 2 ASC;
+
+-- Funcion Max Min
+SELECT 
+	ID_CLIENTE, 
+	MAX(monto_asegurado) AS 'Monto maximo asegurado',
+	MIN(monto_asegurado) AS 'Monto minimo asegurado'
+FROM POLIZA
+GROUP BY ID_CLIENTE
+ORDER BY 1 ASC;
+
+-- HAVING
+SELECT 
+	ID_CLIENTE, 
+	MAX(monto_asegurado) AS 'Monto maximo asegurado',
+	MIN(monto_asegurado) AS 'Monto minimo asegurado'
+FROM POLIZA
+GROUP BY ID_CLIENTE
+HAVING ID_CLIENTE <=10
+ORDER BY 1 ASC;
+
+-- CREATE VIEW
+CREATE VIEW vw_kv_nombre_completo_clientes AS
+SELECT  
+	CONCAT(nombre,' ',apellido) as 'Nombre Completo', 
+	DNI, correo  
+FROM CLIENTE
+WHERE apellido LIKE ('R%') OR apellido LIKE ('%S');
+
+SELECT * FROM vw_kv_nombre_completo_clientes;
+
+/* Combinacion de tablas */
+SELECT 
+	p.ID_CLIENTE, 
+	c.nombre,
+	c.apellido,
+	p.monto_asegurado
+FROM POLIZA p, CLIENTE c
+WHERE P.ID_CLIENTE = C.ID
+GROUP BY P.ID_CLIENTE, C.nombre, C.apellido, P.monto_asegurado
+
+SELECT 
+	p.ID_CLIENTE, 
+	c.nombre,
+	c.apellido,
+	p.monto_asegurado
+FROM POLIZA P
+INNER JOIN CLIENTE C ON P.ID_CLIENTE = C.ID
+ORDER BY 4 ASC
+
+
+--JOINS
+
+-- INNER JOIN
+SELECT
+	CONCAT(A.nombre,' ' , A.apellido) AS 'Nombre del agente',
+	C.ID AS 'ID del cliente'
+FROM CLIENTE C
+INNER JOIN AGENTE A ON A.ID = C.ID_AGENTE
+
+-- LEFT JOIN, RIGHT JOIN, FULL JOIN
+
+
+
