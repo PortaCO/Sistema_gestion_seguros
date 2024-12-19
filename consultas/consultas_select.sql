d6db305a8f9fd2bef0d92b7283a6aa8b4a4847e1
Christian <c.orihuelaporta02@gmail.com>
2024-12-18 19:07:02 -0500

    update

--- consultas/consultas_select.sql
+++ consultas/consultas_select.sql
@@ -24,4 +24,25 @@ SELECT
 FROM CLIENTE
 WHERE apellido LIKE ('R%') OR apellido LIKE ('%S');
 
-select * from cliente
+/* Entontrar filas mediante un operador de comparacion
+Listar los clientes cuyos id sea mayor igual a 15
+*/
+SELECT  *  FROM CLIENTE
+WHERE id >= 15;
+
+-- Listar clientes cuyo id entre 10 y 18
+SELECT  *  FROM CLIENTE
+WHERE ID > 10 AND ID <18;
+
+-- Incluyendo extremos
+SELECT  *  FROM CLIENTE
+WHERE ID BETWEEN 10 AND 18;
+
+-- Seleccionar todos los id diferentes de 10 y diferentes a nombre Jorge
+SELECT * FROM CLIENTE
+WHERE  ID != 10 AND nombre !='Jorge'; 
+
+
+SELECT * FROM CLIENTE
+
+
