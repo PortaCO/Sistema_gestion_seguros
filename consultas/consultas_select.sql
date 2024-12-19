e01f811a137b818e6b91dfcf9ce26d3978187dc9
Christian <c.orihuelaporta02@gmail.com>
2024-12-18 20:07:48 -0500

    update

--- consultas/consultas_select.sql
+++ consultas/consultas_select.sql
@@ -42,7 +42,26 @@ WHERE ID BETWEEN 10 AND 18;
 SELECT * FROM CLIENTE
 WHERE  ID != 10 AND nombre !='Jorge'; 
 
-
+/* Encontrar filas que cumplan cualquier de las 3 condiciones:
+- Su id sea 3
+- Su telefono sea 78901234
+- Que su direccion sea una avenida
+*/
 SELECT * FROM CLIENTE
+WHERE 
+	ID = 3 OR 
+	telefono = 78901234 OR 
+	direccion LIKE ('Av%');
 
+/* Encontrar filas que cumplan varias condiciones:
+- Que su id sea mayor a 10
+- Que su direccion no sea una  avenida
+*/
+SELECT * FROM CLIENTE
+WHERE 
+	id >10 AND
+	direccion NOT LIKE 'Av%';
 
+-- Encontrar filas que no esten en una lista
+SELECT * FROM CLIENTE
+WHERE DNI NOT IN ('12345678', '45678901', '34561210')
